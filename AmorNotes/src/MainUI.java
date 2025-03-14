84c3665eb078e7a81a1c1f104e75c37aab574fdd
ambrocioreg <rga5803@students.uc-bcf.edu.ph>
2025-03-14 08:21:45 +0800

    Added templates of code, based of the UML Diagram.

--- AmorNotes/src/MainUI.java
+++ AmorNotes/src/MainUI.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+
 public class MainUI {
+    public String title;
+    public ArrayList<String> addButton;
+    public boolean searchButton;
+    public boolean toggleGrid;
+    public ArrayList<Card> cards;
+    private Sidebar sidebar;
+    private CloudStorage cloudStorage;
+    private SearchBar searchBar;
+    private Settings settings;
+
+    public MainUI() {
+        this.sidebar = new Sidebar();
+        this.cloudStorage = new CloudStorage();
+        this.searchBar = new SearchBar();
+        this.settings = new Settings();
+    }
+
+    public void addCard(Card card) {}
+    public void editCard(String id) {}
+    public void readCard(String id) {}
+    public void deleteCard(String id) {}
+    public void selectCard(String id) {}
+    public void archiveCard(String id) {}
+    public void toggleSidebar() {}
+    public void toggleSearchBar() {}
+    public void syncToCloud() {}
+
+    
     public static void main(String[] args) {
-        
+        MainUI app = new MainUI();
+        System.out.println("Amor Notes!");
     }
 }
--- AmorNotes/src/Sidebar.java
+++ AmorNotes/src/Sidebar.java
@@ -1,3 +1,7 @@
 public class Sidebar extends MainUI {
-    
+    private boolean optionButton;
+    private boolean userButton;
+
+    public void toggleSettings() {}
+    public void toggleUserProfile() {}
 }
