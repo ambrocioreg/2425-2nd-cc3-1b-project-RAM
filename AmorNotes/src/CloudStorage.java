36ec6a0a7f426f584b25e204c3f9e34a0b22e4dc
rominesgmd <gdr3421@students.uc-bcf.edu.ph>
2025-03-14 08:04:12 +0800

    Add code for the Cardand CloudStorage

--- AmorNotes/src/CloudStorage.java
+++ AmorNotes/src/CloudStorage.java
@@ -1,3 +1,12 @@
 public class CloudStorage extends MainUI {
-    
+    String storageProvider;
+    Boolean IsConnected;
+
+    void connect(){}
+
+    void UploadData(){}
+
+    void downloadData(){}
+
+    void syncWithLocal(){}
 }
