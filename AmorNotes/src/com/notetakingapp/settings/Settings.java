46064b95009750954c86f1a83249202b528fd30e
rominesgmd <gdr3421@students.uc-bcf.edu.ph>
2025-03-20 21:13:11 +0800

    Place all the codes in their proper directory

---
+++ AmorNotes/src/com/notetakingapp/settings/Settings.java
@@ -0,0 +1,8 @@
+public class Settings extends MainUI {
+    private boolean toggleUIColor;
+    private boolean sync;
+
+    public void enableDarkMode() {}
+    public void enableLightMode() {}
+    public void toggleAutoSync() {}
+}
